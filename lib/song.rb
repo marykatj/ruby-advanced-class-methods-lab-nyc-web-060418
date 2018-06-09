088bf2775d1e569f03431a27f25df6c06ab71b80
Mary-Kate James <marykatj@gmail.com>
2018-06-09 19:07:51 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,12 +2,6 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
-  def initialize(name)
-    @name = name
-    @artist_name = artist_name
-    self.class.all << self
-  end
-
   def self.all
     @@all
   end
