3df7f82fdf9b9bd3ffa0879136d35c600961b078
Mary-Kate James <marykatj@gmail.com>
2018-06-09 19:17:51 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,5 +34,10 @@ class Song
 
   def find_or_create_by_name(song_name)
     self.find_by_name(song_name) || self.create_by_name(song_name)
+  end
 
+  def self.alphabetical
+    self.all.sort_by {|song| song.name }
+
+  
 end
