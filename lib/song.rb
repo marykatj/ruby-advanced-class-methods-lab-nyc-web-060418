20eb815352fc4f5a1c7fc4ac1f8eff1507edf483
Mary-Kate James <marykatj@gmail.com>
2018-06-09 19:14:38 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,8 +28,8 @@ class Song
     song
   end
 
-  def self.find_by_name(song)
-    self.all.find {|song| song.name == song }
+  def self.find_by_name(song_name)
+    self.all.find {|song| song.name == song_name }
     return song.self
   end
 
